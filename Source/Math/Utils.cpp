6802ac0bf1d63bda7f4913a8a01f8eff4c9ed10e
Sezz <sezzary@outlook.com>
2025-04-27 02:35:01 +1000

    Progress: math utils

--- Source/Math/Utils.cpp
+++ Source/Math/Utils.cpp
@@ -1,12 +1,11 @@
 #include "Framework.h"
+#include "Math/Utils.h"
+
+#include "Math/Constants.h"
+#include "Math/Objects/Vector3.h"
 
 namespace Silent::Math
 {
-    bool IsApproxEqual(float a, float b, float epsilon)
-    {
-        return (std::max(a, b) - std::min(a, b)) <= epsilon;
-    }
-
     float FloorToStep(float x, float step)
     {
         return floor(x / step) * step;
@@ -26,5 +25,152 @@ namespace Silent::Math
     {
         float alpha = (x - min0) / (max0 - min0);
         return std::lerp(min1, max1, alpha);
+    }
+
+    bool IsApproxEqual(float a, float b, float epsilon)
+    {
+        return (std::max(a, b) - std::min(a, b)) <= epsilon;
+    }
+
+    short GetSurfaceSlopeAngle(const Vector3& normal, const Vector3& axis)
+    {
+        if (normal == -axis)
+        {
+            return 0;
+        }
+
+        float rad = glm::acos(Vector3::Dot(normal, -axis));
+        return FP_ANGLE_FROM_RAD(rad);
+    }
+
+    short GetSurfaceAspectAngle(const Vector3& normal, const Vector3& axis)
+    {
+        if (normal == -axis)
+        {
+            return 0;
+        }
+
+        // Project normal onto plane defined by axis.
+        auto axisNorm = Vector3::Normalize(axis);
+        auto projNormal = normal - (axisNorm * Vector3::Dot(normal, axisNorm));
+
+        // Calculate angle in 2D plane formed by projected normal.
+        float rad = glm::atan2(projNormal.x, projNormal.z);
+        if (Vector3::Dot(projNormal, axisNorm) < 0.0f)
+        {
+            rad += PI;
+        }
+
+        return FP_ANGLE_FROM_RAD(rad);
+    }
+
+    float GetDistanceToLine(const Vector3& from, const Vector3& lineStart, const Vector3& lineEnd)
+    {
+        auto target = GetClosestPointOnLine(from, lineStart, lineEnd);
+        return Vector3::Distance(from, target);
+    }
+
+    Vector3 GetClosestPointOnLine(const Vector3& from, const Vector3& lineStart, const Vector3& lineEnd)
+    {
+        if (lineStart == lineEnd)
+        {
+            return lineStart;
+        }
+
+        auto line = lineEnd - lineStart;
+        float alpha = Vector3::Dot(line, from - lineStart) / Vector3::Dot(line, line);
+
+        // If closest point is out of range, return line start or end point.
+        if (alpha <= 0.0f)
+        {
+            return lineStart;
+        }
+        else if (alpha >= 1.0f)
+        {
+            return lineEnd;
+        }
+
+        // Return closest point on line.
+        return Vector3::Translate(lineStart, line, alpha);
+    }
+
+    Vector3 GetClosestPointOnLinePerp(const Vector3& from, const Vector3& lineStart, const Vector3& lineEnd, const Vector3& axis)
+    {
+        if (lineStart == lineEnd)
+        {
+            return lineStart;
+        }
+
+        // Ensure axis is normalized.
+        auto axisNorm = axis;
+        axisNorm.Normalize();
+
+        // Compute line.
+        auto line = lineEnd - lineStart;
+
+        // Project line and from onto plane perpendicular to input axis.
+        auto linePerp = line - (axisNorm * Vector3::Dot(line, axisNorm));
+        auto fromPerp = from - (axisNorm * Vector3::Dot(from, axisNorm));
+
+        // Compute alpha from line projection.
+        float alpha = Vector3::Dot(linePerp, fromPerp - lineStart) / Vector3::Dot(linePerp, linePerp);
+
+        // If closest point is out of range, return start or end point.
+        if (alpha <= 0.0f)
+        {
+            return lineStart;
+        }
+        else if (alpha >= 1.0f)
+        {
+            return lineEnd;
+        }
+
+        // Return closest point on line perpendicular to input axis.
+        return Vector3::Translate(lineStart, line, alpha);
+    }
+
+    bool IsPointInFront(const Vector3& origin, const Vector3& target, const Vector3& ref, const Vector3& axis)
+    {
+        if (origin == target)
+        {
+            return true;
+        }
+
+        auto refDir = ref - origin;
+
+        // Project `refDir` onto plane defined by axis.
+        auto axisNorm = Vector3::Normalize(axis);
+        auto projRefDir = refDir - (axisNorm * Vector3::Dot(refDir, axisNorm));
+    
+        // Project target direction onto same plane.
+        auto targetDir = target - origin;
+        auto projTargetDir = targetDir - (axisNorm * Vector3::Dot(targetDir, axisNorm));
+    
+        float dot = Vector3::Dot(projRefDir, projTargetDir);
+        return dot > 0.0f;
+    }
+
+    bool IsPointOnLeft(const Vector3& origin, const Vector3& target, const Vector3& ref, const Vector3& axis)
+    {
+        if (origin == target)
+        {
+            return true;
+        }
+
+        auto refDir = ref - origin;
+
+        // Project `refDir` onto plane orthogonal to axis.
+        auto axisNorm = Vector3::Normalize(axis);
+        auto projRefDir = refDir - (axisNorm * Vector3::Dot(refDir, axisNorm));
+
+        // Normal vector to projected reference direction in 2D.
+        auto headingNormal = Vector3(projRefDir.z, 0.0f, -projRefDir.x);
+
+        // Project `targetDir` onto same plane.
+        auto targetDir = target - origin;
+        auto projTargetDir = targetDir - (axisNorm * Vector3::Dot(targetDir, axisNorm));
+
+        float dot = Vector3::Dot(headingNormal, projTargetDir);
+        return dot > 0.0f;
     }
 }
--- Source/Math/Utils.h
+++ Source/Math/Utils.h
@@ -1,12 +1,26 @@
 #pragma once
 
 #include "Math/Constants.h"
+#include "Math/Objects/Vector3.h"
 
 namespace Silent::Math
 {
-    bool  IsApproxEqual(float a, float b, float epsilon = EPSILON);
+    // Numeric
+
     float FloorToStep(float x, float step);
     float CeilToStep(float x, float step);
     float RoundToStep(float x, float step);
     float Remap(float x, float min0, float max0, float min1, float max1);
+    bool  IsApproxEqual(float a, float b, float epsilon = EPSILON);
+
+    // Geometric
+
+	short   GetSurfaceSlopeAngle(const Vector3& normal, const Vector3& axis = Vector3::UnitY);
+	short   GetSurfaceAspectAngle(const Vector3& normal, const Vector3& axis = Vector3::UnitY);
+	float	GetDistanceToLine(const Vector3& from, const Vector3& lineStart, const Vector3& lineEnd);
+	Vector3 GetClosestPointOnLine(const Vector3& from, const Vector3& lineStart, const Vector3& lineEnd);
+	Vector3 GetClosestPointOnLinePerp(const Vector3& from, const Vector3& lineStart, const Vector3& lineEnd, const Vector3& axis = Vector3::UnitY);
+
+	bool IsPointInFront(const Vector3& from, const Vector3& to, const Vector3& ref, const Vector3& axis = Vector3::UnitY);
+	bool IsPointOnLeft(const Vector3& from, const Vector3& to, const Vector3& ref, const Vector3& axis = Vector3::UnitY);
 }
