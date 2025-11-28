baed7a86a0930eab2a65890f210ae435d9abc087
Sezz <sezzary@outlook.com>
2025-11-29 02:41:31 +1100

    GUI button progress

--- Source/Gui/Button.cpp
+++ Source/Gui/Button.cpp
@@ -1,55 +1,158 @@
 #include "Framework.h"
 #include "Gui/Button.h"
 
+#include "Application.h"
 #include "Input/Input.h"
+#include "Renderer/Common/Enums.h"
+#include "Utils/Utils.h"
 
 using namespace Silent::Input;
+using namespace Silent::Renderer;
+using namespace Silent::Utils;
 
 namespace Silent::Gui
 {
-    Button::Button(const Rectangle& bounds,
+    Button::Button(const Rectangle& bounds, ScaleMode scaleMode,
                    std::optional<Callback> onEnter,
                    std::optional<Callback> onInside,
                    std::optional<Callback> onLeave,
+                   std::optional<Callback> onOutside,
                    std::optional<Callback> onClick,
                    std::optional<Callback> onHold,
                    std::optional<Callback> onRelease)
     {
         _bounds    = bounds;
+        _scaleMode = scaleMode;
+        _isActive  = false;
+
         _onEnter   = onEnter.value_or(nullptr);
         _onInside  = onInside.value_or(nullptr);
         _onLeave   = onLeave.value_or(nullptr);
+        _onOutside = onOutside.value_or(nullptr);
         _onClick   = onClick.value_or(nullptr);
         _onHold    = onHold.value_or(nullptr);
         _onRelease = onRelease.value_or(nullptr);
     }
 
+    void Button::Update(bool isFocused)
+    {
+        constexpr auto SELECT_ACTION_IDS = std::array<ActionId, 2>
+        {
+            In::Enter,
+            In::Action
+        };
+
+        Update(isFocused, ToSpan(SELECT_ACTION_IDS));
+    }
+
     void Button::Update(const Vector2& point)
     {
-        if (!_bounds.Intersects(point))
+        constexpr auto SELECT_ACTION_IDS = std::array<ActionId, 1>
         {
-            return;
+            In::MouseClickLeft
+        };
+
+        Update(_bounds.Intersects(point), ToSpan(SELECT_ACTION_IDS));
+    }
+
+    bool Button::CheckClickedAction(const std::span<const ActionId>& actionIds) const
+    {
+        const auto& input = g_App.GetInput();
+
+        for (auto actionId : actionIds)
+        {
+            if (input.GetAction(actionId).IsClicked())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool Button::CheckHeldAction(const std::span<const ActionId>& actionIds) const
+    {
+        const auto& input = g_App.GetInput();
+
+        for (auto actionId : actionIds)
+        {
+            if (input.GetAction(actionId).IsHeld())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool Button::CheckReleasedAction(const std::span<const ActionId>& actionIds) const
+    {
+        const auto& input = g_App.GetInput();
+
+        for (auto actionId : actionIds)
+        {
+            if (input.GetAction(actionId).IsReleased())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void Button::Update(bool isActive, const std::span<const ActionId>& selectActionIds)
+    {
+        if (isActive)
+        {
+            // Execute On Enter callback if previous active state was outside.
+            if (!_isActive)
+            {
+                ExecuteCallback(_onEnter);
+                _isActive = true;
+            }
+            // Execute On Inside callback if previous active state was also inside.
+            else
+            {
+                ExecuteCallback(_onInside);
+            }
+
+            // Execute On Click callback if select action is clicked.
+            if (CheckClickedAction(selectActionIds))
+            {
+                ExecuteCallback(_onClick);
+            }
+            // Execute On Hold callback if select action is held.
+            else if (CheckHeldAction(selectActionIds))
+            {
+                ExecuteCallback(_onHold);
+            }
+            // Execute On Release callback if select action is released.
+            else if (CheckReleasedAction(selectActionIds))
+            {
+                ExecuteCallback(_onRelease);
+            }
+        }
+        else
+        {
+            // Execute On Leave callback if previous active state was inside.
+            if (_isActive)
+            {
+                ExecuteCallback(_onLeave);
+                _isActive = false;
+            }
+            // Execute On Outside callback if previous active state was also outside.
+            else
+            {
+                ExecuteCallback(_onOutside);
+            }
         }
     }
 
-    void Button::HandleEnter(const Vector2& point) const
+    void Button::ExecuteCallback(const std::optional<Callback>& callback) const
     {
-        // Check if hover is valid.
-        if (!_onEnter)
+        if (callback.has_value())
         {
-            return;
+            (*callback)();
         }
-
-        _onEnter();
-    }
-
-    void Button::HandleClick(const Vector2& point) const
-    {
-        if (!_onClick)
-        {
-            return;
-        }
-
-        _onClick();
     }
 }
--- Source/Gui/Button.h
+++ Source/Gui/Button.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include "Input/Input.h"
+#include "Renderer/Common/Enums.h"
+
+using namespace Silent::Input;
+using namespace Silent::Renderer;
+
 namespace Silent::Gui
 {
-    /** @brief Clickable screen-space region. */
+    /** @brief Interactable screen space region. */
     class Button
     {
     private:
@@ -16,26 +22,41 @@ namespace Silent::Gui
         // Fields
         // =======
 
+        bool _isActive  = false;
+
         Rectangle _bounds    = Rectangle();
+        ScaleMode _scaleMode = ScaleMode::Fit;
+
         Callback  _onEnter   = nullptr;
         Callback  _onInside  = nullptr;
         Callback  _onLeave   = nullptr;
+        Callback  _onOutside = nullptr;
         Callback  _onClick   = nullptr;
         Callback  _onHold    = nullptr;
         Callback  _onRelease = nullptr;
-
-        bool _isInside  = false;
-        bool _isOutside = false;
 
     public:
         // =============
         // Constructors
         // =============
 
-        Button(const Rectangle& bounds,
+        /** @brief Constructs a `Button` with bounds and interaction callbacks.
+         *
+         * @param bounds 2D rectangle representing interaction bounds.
+         * @param scaleMode Renderer scale mode.
+         * @param onEnter Callback to execute on enter.
+         * @param onInside Callback to execute on inside.
+         * @param onLeave Callback to execute on leave.
+         * @param onOutside Callback to execute on outside.
+         * @param onClick Callback to execute on clicked input.
+         * @param onHold Callback to execute on held input.
+         * @param onRelease Callback to execute on released input.
+         */
+        Button(const Rectangle& bounds, Silent::Renderer::ScaleMode scaleMode,
                std::optional<Callback> onEnter,
                std::optional<Callback> onInside,
                std::optional<Callback> onLeave,
+               std::optional<Callback> onOutside,
                std::optional<Callback> onClick,
                std::optional<Callback> onHold,
                std::optional<Callback> onRelease);
@@ -44,6 +65,16 @@ namespace Silent::Gui
         // Utilities
         // ==========
 
+        /** @brief Updates the button state using a discrete focus state from keyboard or controller button navigation and executes callbacks.
+         *
+         * @param isFocused `true` if the button is focused, `false` otherwise.
+         */
+        void Update(bool isFocused);
+
+        /** @brief Updates the button state using a screen space point from mouse or controller cursor navigation and executes callbacks.
+         *
+         * @param point Screen space point.
+         */
         void Update(const Vector2& point);
 
     private:
@@ -51,8 +82,35 @@ namespace Silent::Gui
         // Helpers
         // ========
 
-        void HandleEnter(const Vector2& point) const;
+        /** @brief Checks if an input action in a given list is clicked.
+         *
+         * @return `true` if an action is clicked, `false` otherwise.
+         */
+        bool CheckClickedAction(const std::span<const ActionId>& actionIds) const;
 
-        void HandleClick(const Vector2& point) const;
+        /** @brief Checks if an input action in a given list is held.
+         *
+         * @return `true` if an action is held, `false` otherwise.
+         */
+        bool CheckHeldAction(const std::span<const ActionId>& actionIds) const;
+
+        /** @brief Checks if an input action in a given list is released.
+         *
+         * @return `true` if an action is released, `false` otherwise.
+         */
+        bool CheckReleasedAction(const std::span<const ActionId>& actionIds) const;
+
+        /** @brief Updates the button state using a screen space point from mouse or controller cursor navigation and executes callbacks.
+         *
+         * @param isActive `true` if the button is active, `false` otherwise.
+         * @param selectActionIds Input action IDs used for selection.
+         */
+        void Update(bool isActive, const std::span<const ActionId>& selectActionIds);
+
+        /** @brief Executes an interaction callback.
+         *
+         * @param callback Interaction callback.
+         */
+        void ExecuteCallback(const std::optional<Callback>& callback) const;
     };
 }
