a48b43f0fa47b05238ae71ee6e847905b353c58c
Sezz <sezzary@outlook.com>
2025-11-30 03:49:21 +1100

    Menu port progress

--- Source/Game/Screens/Options/MenuGraphics.h
+++ Source/Game/Screens/Options/MenuGraphics.h
@@ -16,28 +16,28 @@ namespace Silent::Game
      */
     void Options_MainOptionsMenu_VolumeBarDraw(bool isSfx, uchar vol);
 
-    /** @brief Draws the heading and all listed entry strings in the extra options menu. */
-    void Options_ExtraOptionsMenu_EntryStringsDraw();
-
     /** @brief Draws the heading and all listed entry strings in the main options menu. */
     void Options_MainOptionsMenu_EntryStringsDraw();
 
-    /** @brief Draws gold bullet points next to the listed entries and a highlight indicating the
-     * selected entry in the extra options menu.
-     */
-    void Options_ExtraOptionsMenu_SelectionHighlightDraw();
+    /** @brief Draws the heading and all listed entry strings in the extra options menu. */
+    void Options_ExtraOptionsMenu_EntryStringsDraw();
 
     /** @brief Draws gold bullet points next to the listed entries and a highlight indicating the
      * selected entry in the main options menu.
      */
     void Options_MainOptionsMenu_SelectionHighlightDraw();
 
+    /** @brief Draws gold bullet points next to the listed entries and a highlight indicating the
+     * selected entry in the extra options menu.
+     */
+    void Options_ExtraOptionsMenu_SelectionHighlightDraw();
+
     /** @brief Draws the background vignette of Harry in the main and extra options menus. */
     void Options_Menu_VignetteDraw();
 
-    /** @brief Draws configuration strings and blue arrows to the right of the listed entries in the extra options menu. */
-    void Options_ExtraOptionsMenu_ConfigDraw();
-
     /** @brief Draws configuration strings and blue arrows to the right of the listed entries in the main options menu. */
     void Options_MainOptionsMenu_ConfigDraw();
+
+    /** @brief Draws configuration strings and blue arrows to the right of the listed entries in the extra options menu. */
+    void Options_ExtraOptionsMenu_ConfigDraw();
 }
