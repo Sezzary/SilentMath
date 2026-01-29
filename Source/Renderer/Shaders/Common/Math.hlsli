3886b2bce45a1b37e608aa89bc97d319590505a4
Sezz <sezzary@outlook.com>
2026-01-29 19:27:31 +1100

    Update PSX dither shader

--- Source/Renderer/Shaders/Common/Math.hlsli
+++ Source/Renderer/Shaders/Common/Math.hlsli
@@ -2,6 +2,7 @@
 
 namespace Math
 {
+    static const uint   UINT8_MAX  = 255;
     static const float3 LUMA_BT601 = float3(0.299f, 0.587f, 0.114f);
 
     float Remap(float val, float fromMin, float fromMax, float toMin, float toMax)
--- Source/Renderer/Shaders/Dither.frag.hlsl
+++ Source/Renderer/Shaders/Dither.frag.hlsl
@@ -1,3 +1,5 @@
+#include "Common/Math.hlsli"
+
 // References:
 // https://gist.github.com/ompuco/3209f1b32213cec5b7bccf0e67caf3e9
 
@@ -21,19 +23,23 @@ static const int4x4 PSX_DITHER_TABLE = int4x4
 
 float4 main(Input input) : SV_Target
 {
+    static const int  DITHER_SIZE = 4;
+    static const uint DITHER_CEIL = 0xF8;
+
     // Sample texture.
     float4 texColor = Texture.Sample(Sampler, input.TextureCoord);
 
     // Compute pixel position.
     int2 pixelPos = int2(floor(input.Position.xy));
 
-    // Dithered color.
-    int    dither    = PSX_DITHER_TABLE[pixelPos.x % 4][pixelPos.y % 4];
-    float3 color8Bit = texColor.rgb * 255.0f;
+    // Compute 8-bit dithered color.
+    int    dither    = PSX_DITHER_TABLE[pixelPos.x % DITHER_SIZE][pixelPos.y % DITHER_SIZE];
+    float3 color8Bit = texColor.rgb * float(Math::UINT8_MAX);
     color8Bit       += (dither / 2.0f) - 4.0f;
-    color8Bit        = lerp((uint3(color8Bit) & 0xF8), 0xF8, step(0xF8, color8Bit));
-    color8Bit       /= 255.0f;
 
-    // Compute funal color.
-    return float4(color8Bit, 1.0f);
+    // 5-bit color truncation.
+    color8Bit = lerp((uint3(color8Bit) & DITHER_CEIL), DITHER_CEIL, step(DITHER_CEIL, color8Bit));
+
+    // Compute final color.
+    return float4(color8Bit / float(Math::UINT8_MAX), 1.0f);
 }
