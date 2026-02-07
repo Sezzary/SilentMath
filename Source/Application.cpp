2989416e98849e4dceee2ccc712be7c480272ed6
Sezz <sezzary@outlook.com>
2026-02-07 21:03:10 +1100

    Update Application.cpp

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -192,28 +192,6 @@ namespace Silent
         _work.Toaster.Add(_work.Translator((PLATFORM_TYPE == PlatformType::MacOs) ? KEY_SYS_FULLSCREEN_HINT_MAC : KEY_SYS_FULLSCREEN_HINT_GENERIC));
 
         Debug::Log("Startup complete.");
-
-        const auto& asset1 = _work.Assets.GetAsset("ITEM/UNQE1.TMD");
-        //const auto data = asset->GetData<TmdAsset>();
-        //Debug::Log(Fmt("Meshes: {}", data->Meshes.size()));
-        //Debug::Log(Fmt("Prims: {}", data->Meshes[0].Primitives.size()));
-        //Debug::Log(Fmt("Positions: {}", data->Meshes[0].Positions.size()));
-        //Debug::Log(Fmt("Normals: {}", data->Meshes[0].Normals.size()));
-
-        const auto& asset = _work.Assets.GetAsset("ANIM/CAT.ANM");
-        const auto data = asset->GetData<AnmAsset>();
-        //Debug::Log(Fmt("Meshes: {}", data->Meshes.size()));
-        //Debug::Log(Fmt("Prims: {}", data->Meshes[0].Primitives.size()));
-        //Debug::Log(Fmt("Positions: {}", data->Meshes[0].Positions.size()));
-        //Debug::Log(Fmt("Normals: {}", data->Meshes[0].Normals.size()));
-
-        const auto& asset2 = _work.Assets.GetAsset("CHARA/DOC.ILM");
-        const auto data2 = asset2->GetData<IlmAsset>();
-        Debug::Log(Fmt("Meshes: {}", data2->Meshes.size()));
-        Debug::Log(Fmt("Prims: {}", data2->Meshes[0].Primitives.size()));
-        Debug::Log(Fmt("Positions: {}", data2->Meshes[0].Positions.size()));
-        Debug::Log(Fmt("Normals: {}", data2->Meshes[0].Normals.size()));
-        Debug::Log(Fmt("UVs: {}", data2->Meshes[0].Uvs.size()));
     }
 
     void ApplicationManager::Deinitialize()
