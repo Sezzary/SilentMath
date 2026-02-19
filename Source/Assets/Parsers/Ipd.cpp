a6ae1b9fca52924c545ceb5d4c455cd812963571
Sezz <sezzary@outlook.com>
2026-02-19 15:29:54 +1100

    Port sin/cos table and sin/cos functions, IPD parser progress

--- Source/Assets/Parsers/Ipd.cpp
+++ Source/Assets/Parsers/Ipd.cpp
@@ -9,9 +9,17 @@ using namespace Silent::Utils;
 
 namespace Silent::Assets
 {
+    struct IpdModelInfo
+    {
+        uint8       IsGlobalPlm       = false;
+        std::string Name              = {};
+        uint32      ModelHeaderOffset = 0;
+    };
+
     std::shared_ptr<void> ParseIpd(const std::filesystem::path& filename)
     {
-        constexpr uint8 HEADER_MAGIC = 0x14;
+        constexpr uint8 IPD_HEADER_MAGIC = 0x14;
+        constexpr uint8 LM_HEADER_MAGIC  = 0x30;
 
         const auto& fs = g_App.GetFilesystem();
 
@@ -27,7 +35,7 @@ namespace Silent::Assets
 
         // Read header magic.
         uint8 magic = stream.ReadUint8();
-        if (magic != HEADER_MAGIC)
+        if (magic != IPD_HEADER_MAGIC)
         {
             throw std::runtime_error(Fmt("Failed to parse invalid IPD `{}`.",
                                          std::filesystem::relative(filename, fs.GetAssetsDirectory()).string()));
@@ -42,13 +50,35 @@ namespace Silent::Assets
         uint8  modelBufferCount    = stream.ReadUint8();
         uint8  modelOrderCount     = stream.ReadUint8();
         stream.Skip(9); // Unknown.
-        uint32 modelInfoOffset     = stream.ReadUint32();
+        uint32 modelInfosOffset    = stream.ReadUint32();
         uint32 modelBuffersOffset  = stream.ReadUint32();
         uint8  texsOffset          = stream.ReadUint8();
         stream.Skip(51); // Unknown.
         uint32 modelOrderPtrOffset = stream.ReadUint32();
 
-        // @todo
+        stream.Skip(52); // Collision.
+
+        // Read LM header magic.
+        stream.SetPosition(lmHeaderOffset);
+        uint32 lmMagic = stream.ReadUint8();
+        if (lmMagic != LM_HEADER_MAGIC)
+        {
+            throw std::runtime_error(Fmt("Failed to parse invalid IPD `{}`.",
+                                         std::filesystem::relative(filename, fs.GetAssetsDirectory()).string()));
+        }
+
+        // Read model infos.
+        auto modelInfos = std::vector<IpdModelInfo>{};
+        modelInfos.reserve(modelCount);
+        stream.SetPosition(modelInfosOffset);
+        for (int i = 0; i < modelCount; i++)
+        {
+            auto modelInfo              = IpdModelInfo{};
+            modelInfo.IsGlobalPlm       = stream.ReadUint8();
+            modelInfo.Name              = stream.ReadNullString(8);
+            modelInfo.ModelHeaderOffset = stream.ReadUint32();
+            modelInfos.push_back(std::move(modelInfo));
+        }
 
         return std::make_shared<IpdAsset>(std::move(asset));
     }
