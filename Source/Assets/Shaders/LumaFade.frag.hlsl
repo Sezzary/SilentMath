d197de57cd8daf35c6b0b40c657755518422fedf
Sezz <sezzary@outlook.com>
2026-02-27 14:40:55 +1100

    Fix inverted luma fade

--- Source/Assets/Shaders/LumaFade.frag.hlsl
+++ Source/Assets/Shaders/LumaFade.frag.hlsl
@@ -19,11 +19,11 @@ cbuffer PerFrame : register(b0, space3)
 float4 main(Input input) : SV_Target
 {
     float4 texColor     = Texture.Sample(Sampler, input.TexCoord);
-    float3 workingColor = IsWhite ? texColor.rgb : (1.0f - texColor.rgb);
+    float3 workingColor = IsWhite ? (1.0f - texColor.rgb) : texColor.rgb;
 
     float  luma       = 1.0f - dot(workingColor, Math::LUMA_BT601);
     float3 fadedColor = workingColor * Math::Remap(1.0f - FadeAlpha, luma, 1.0f, 0.0f, 1.0f);
 
-    float3 finalColor = IsWhite ? fadedColor : (1.0f - fadedColor);
+    float3 finalColor = IsWhite ? (1.0f - fadedColor) : fadedColor;
     return float4(finalColor, 1.0f);
 }
