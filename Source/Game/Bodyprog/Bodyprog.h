2123ec99ce7120f9d21deb9c0a2a5d5b728be062
Sezz <sezzary@outlook.com>
2026-03-19 14:45:47 +1100

    Sync deltas

--- Source/Game/Bodyprog/Bodyprog.h
+++ Source/Game/Bodyprog/Bodyprog.h
@@ -1532,7 +1532,7 @@ namespace Silent::Game
         s_MapOverlayHeader_94* field_94;                    // only map1_s02, map1_s03.
         //s32                    (*func_98)(POLY_FT4** poly, s32); // only map1_s02, map1_s03.
         void                   (*func_9C)();                // only map1_s02, map1_s03.
-        s32*                   func_A0; // only map1_s03.
+        void*                  ptr_A0; // M1S03 only, pointer to `s_800E3A40` array.
         //s32                    (*func_A4)(POLY_FT4** poly, s32); // func(?) only map1_s03.
         void                   (*func_A8)(); // func(?) only map6_s02.
         //s32                    (*func_AC)(POLY_FT4** poly, s32); // func(?) only map4_s03, map4_s05.
@@ -1568,16 +1568,16 @@ namespace Silent::Game
         void                   (*func_124)(s_SubCharacter*); // Assumed return type.
         s32                    (*func_128)(s_SubCharacter*); // Assumed return type.
         s32                    (*func_12C)(s_SubCharacter*); // Assumed return type.
-        void                   (*func_130)(); // func(?).
+        void                   (*func_130)(s_SubCharacter*);
         s32                    (*func_134)(s_SubCharacter*); // Assumed return type.
         s32                    (*func_138)(s_SubCharacter*); // Keyframe state getter. Return value depends on the anim update function being used.
-        s32                    (*func_13C)(s32, s32, void*, s16, s32); // `arg0` is `s_SubCharacter*`.
-        void                   (*func_140)(); // func(?).
-        void                   (*func_144)(); // func(?).
-        void                   (*func_148)(); // func(?).
-        void                   (*func_14C)(); // func(?).
-        void                   (*animStartKeyframeIdxGet_150)();
-        void                   (*func_154)(); // func(?).
+        bool                   (*func_13C)(s_SubCharacter* chara, s32 arg1, VECTOR3* arg2In, s32 angleIn, s32 arg4); // `arg0` is `s_SubCharacter*`.
+        void                   (*func_140)(s_SubCharacter* chara);
+        void                   (*func_144)(s_SubCharacter* chara);
+        bool                   (*func_148)(s32 animStatus, s_SubCharacter* chara, s32 keyframeIdx0, s32 keyframeIdx1, s32 sfxId, s32 pitch);
+        bool                   (*func_14C)(s32 animStatus, s_SubCharacter* chara, s32 keyframeIdx, s32 sfxId);
+        s32                    (*animStartKeyframeIdxGet_150)(s_SubCharacter* chara);
+        void                   (*func_154)(s_SubCharacter* chara);
         void                   (*func_158)(q19_12 x, q19_12 z); // only map1_s05, map1_s06.
         void                   (*func_15C)(); // func(?) only map5_s01.
         void                   (*func_160)(); // func(?) only map5_s01.
