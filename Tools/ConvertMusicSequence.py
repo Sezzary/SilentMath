31016695c72882cabb5504edd03d2b93fd33d061
Sezz <sezzary@outlook.com>
2026-03-20 23:05:01 +1100

    AMPEG sustain conversion fixup

--- Tools/ConvertMusicSequence.py
+++ Tools/ConvertMusicSequence.py
@@ -103,7 +103,7 @@ class ToneAttr:
     adsr1:     int
     adsr2:     int
     prog:      int
-    vag:       int
+    vag_id:    int
 
 # VAB program attributes.
 @dataclass
@@ -214,7 +214,7 @@ def _extract_vab_samples_to_wav(vgmstream_exe: Path, output_folder: Path, vab_fi
         logging.info(f"Extracting sample {i}...")
 
         # Run conversion command.
-        wav_file = output_folder / vab_file.stem / SAMPLES_FOLDER / f"{VAG_PREFIX}{i:003}.WAV"
+        wav_file = output_folder / vab_file.stem / SAMPLES_FOLDER / f"{VAG_PREFIX}{i:03}.WAV"
         command = [
             vgmstream_exe.resolve(),
             "-s", str(i),
@@ -277,27 +277,35 @@ def _build_sfz_from_vab(output_folder: Path, vab_file: Path):
     :param output_folder: Directory where the `SFZ` will be saved.
     :param vab_file: The source `VAB` to convert.
     """
-    def psx_adsr_to_sec(rate, is_release=False):
+    def convert_psx_adsr_to_sec(rate, is_release=False):
         """
-        @todo
+        Convert PSX SPU ADSR envelopes to seconds.
 
-        :param rate: 
-        :param is_release: 
+        The PS1 SPU calculates envelope rates exponentially. For release,  each increment of 1 in the VAB rate doubles
+        the duration in seconds. For attack and decay, the rate is scaled to provide a smooth millisecond-based
+        transition.
+
+        :param rate: The raw 5-bit (release) or 7-bit (attack/decay) value.
+        :param is_release: `True` if processing the release stage.
+        :return: The duration in seconds, rounded to 3 decimal places.
         """
         if rate <= 0:
             return 0.0
 
-        # Simulate specific PS1 SPU power-of-two curve.
-        return round(0.001 * (2 ** (rate / 2)), 3) if not is_release else round(0.001 * (2 ** ((31 - rate) / 2)) * 32, 3)
+        if is_release:
+            return round(0.0001479 * (2 ** rate), 3)
+
+        return round(0.001 * (2 ** (rate / 4)), 3)
 
     def get_wav_loop_points(wav_file: Path):
         """
         Extract the loop start/end points from the `smpl` chunk of a `WAV`.
 
         :param wav_file: The source `WAV` to process.
+        :return: The start/end loop points.
         """
         with open(wav_file, "rb") as _file:
-            _file.read(12) # Skip RIFF header.
+            _file.read(12) # Skip `RIFF` header.
 
             while True:
                 try:
@@ -325,19 +333,19 @@ def _build_sfz_from_vab(output_folder: Path, vab_file: Path):
     if not parsed_vab:
         return
 
-    header, programs = parsed_vab
-    sfz_file         = output_folder / vab_file.stem / f"{vab_file.stem}.sfz"
+    header, progs = parsed_vab
+    sfz_file      = output_folder / vab_file.stem / f"{vab_file.stem}.sfz"
 
     with open(sfz_file, 'w') as output:
-        for prog_idx, prog in enumerate(programs):
+        for prog_idx, prog in enumerate(progs):
             for tone in prog.tones:
-                wav_name = f"{SAMPLES_FOLDER}/{VAG_PREFIX}{tone.vag:03}.WAV"
+                wav_name = f"{SAMPLES_FOLDER}/{VAG_PREFIX}{tone.vag_id:03}.WAV"
 
                 # ADSR mapping.
-                ar = (tone.adsr1 >> 10) & 0x1F
-                dr = (tone.adsr1 >> 6) & 0x0F
-                sl = tone.adsr1 & 0x0F
-                rr = tone.adsr2 & 0x1F
+                ar = (tone.adsr1 >> 8) & 0x7F # Attack 7 bits (0-127).
+                dr = (tone.adsr1 >> 4) & 0x0F # Decay 4 bits (0-15).
+                sl = tone.adsr1 & 0x0F        # Sustain level 4 bits (0-15).
+                rr = tone.adsr2 & 0x1F        # Release 5 bits (0-31).
 
                 # Get sample loop points.
                 wav_file                 = output_folder / vab_file.stem / wav_name
@@ -358,6 +366,7 @@ def _build_sfz_from_vab(output_folder: Path, vab_file: Path):
                     "sample":          wav_name,
                     "bank":            0,
                     "program":         prog_idx,
+                    #"offset":          28, @todo `WAV`s have 28 samples of padding at the start.
                     "loop_mode":       "loop_continuous" if tone.mode == 4 else "one_shot",
                     "loop_start":      sample_start,
                     "loop_end":        sample_end,
@@ -365,144 +374,19 @@ def _build_sfz_from_vab(output_folder: Path, vab_file: Path):
                     "hikey":           tone.max_note,
                     "pitch_keycenter": tone.center,
                     "volume":          round(20 * math.log10(max(tone.vol, 1) / 127), 2),
-                    "pan":             round((tone.pan - 64) / 64 * 100),
+                    "pan":             round(((tone.pan - 64) / 64) * 100),
                     "tune":            round((tone.shift / 128) * 100),
-                    "ampeg_attack":    psx_adsr_to_sec(ar),
-                    "ampeg_decay":     psx_adsr_to_sec(dr),
+                    "ampeg_attack":    convert_psx_adsr_to_sec(ar),
+                    "ampeg_decay":     convert_psx_adsr_to_sec(dr),
                     "ampeg_sustain":   round((sl / 15) * 100, 2),
-                    "ampeg_release":   psx_adsr_to_sec(rr, is_release=True),
+                    "ampeg_release":   convert_psx_adsr_to_sec(rr, is_release=True),
+                    "bend_up":         tone.pb_max * 100,
+                    "bend_down":       tone.pb_min * 100,
+                    "pitchlfo_freq":   round((tone.vib_t / 127) * 10, 2),
+                    "pitchlfo_depth":  round((tone.vib_w / 127) * 50, 2),
                 }
                 output.write(" ".join([f"{key}={value}" for key, value in region_data.items()]))
                 output.write("\n\n")
-
-def _build_sfz_from_vab_OLD(output_folder: Path, vab_file: Path):
-    def convert_psx_rate_to_sec(rate, max_val=0x1F):
-        """
-        Approximate a PSX SPU rate to `SFZ` seconds.
-
-        :param rate: PSX SPU rate to convert.
-        """
-        if rate == 0:
-            return 0.0
-
-        # Higher rate = faster time. This creates a usable curve.
-        return round(0.001 * (2 ** ((max_val - rate) / 2)), 3)
-
-    def convert_psx_sl_to_percent(sustain):
-        """
-        Convert a 4-bit sustain level `[0, 15]` to `SFZ` percentage `[0, 100]`.
-
-        :param sustain: PSX sustain level to convert.
-        """
-        return round((sustain / 15) * 100, 2)
-
-    logging.info(f"Building `SFZ` from `{vab_file.name}`...")
-
-    with vab_file.open("rb") as _file:
-        header = _file.read(32)
-        magic, ver, vab_id, file_size, res0, prog_count, tone_count, vag_count = struct.unpack('<4sIIiHHHH', header[:24])
-
-        if magic != b'pBAV':
-            logging.error(f"Invalid `VAB`.")
-            return
-
-        # Collect programs.
-        prog_tone_counts = []
-        for i in range(128):
-            prog_data  = _file.read(16)
-            tone_count = prog_data[0]
-            prog_tone_counts.append(tone_count)
-
-        # Collect program tones.
-        tones = []
-        for i in range(prog_count):
-            active_tones = prog_tone_counts[i]
-
-            for tone_idx in range(16):
-                raw_tone = _file.read(32)
-
-                # Map tones of active programs.
-                if active_tones > 0 and tone_idx < active_tones:
-                    tone = VgaTone(
-                        program_idx=i,
-                        vag_id=struct.unpack('<h', raw_tone[VgaToneDataIdx.VAG_ID:VgaToneDataIdx.VAG_ID + 2])[0],
-                        center=raw_tone[VgaToneDataIdx.CENTER],
-                        note_min=raw_tone[VgaToneDataIdx.NOTE_MIN],
-                        note_max=raw_tone[VgaToneDataIdx.NOTE_MAX],
-                        vibrato_width=raw_tone[VgaToneDataIdx.VIBRATO_WIDTH],
-                        vibrato_time=raw_tone[VgaToneDataIdx.VIBRATO_TIME],
-                        portamento_width=raw_tone[VgaToneDataIdx.PORTAMENTO_WIDTH],
-                        portamento_time=raw_tone[VgaToneDataIdx.PORTAMENTO_TIME],
-                        pitch_bend_min=-raw_tone[VgaToneDataIdx.PITCH_BEND_MIN],
-                        pitch_bend_max=raw_tone[VgaToneDataIdx.PITCH_BEND_MAX],
-                        panning=raw_tone[VgaToneDataIdx.PANNING],
-                        shift=raw_tone[VgaToneDataIdx.SHIFT],
-                        volume=raw_tone[VgaToneDataIdx.VOLUME],
-                        mode=raw_tone[VgaToneDataIdx.MODE],
-                        adsr1=struct.unpack('<H', raw_tone[VgaToneDataIdx.ADSR1:VgaToneDataIdx.ADSR1 + 2])[0],
-                        adsr2=struct.unpack('<H', raw_tone[VgaToneDataIdx.ADSR2:VgaToneDataIdx.ADSR2 + 2])[0]
-                    )
-                    tones.append(tone)
-
-        # Write tones.
-        sfz_file = output_folder / vab_file.stem / f"{vab_file.stem}.SFZ"
-        with open(sfz_file, 'w') as output:
-            for tone in tones:
-                # Extract values
-                ar = (tone.adsr1 >> 10) & 0x1F
-                dr = (tone.adsr1 >> 4)  & 0x0F
-                sl = tone.adsr1         & 0x0F
-                sr = (tone.adsr2 >> 6)  & 0x7F
-                rr = tone.adsr2         & 0x1F
-
-                sfz_vibrato_freq    = (tone.vibrato_time / 127) * 10    # VAB `[0, 127]` -> SFZ hertz.
-                sfz_vibrato_depth   = (tone.vibrato_width / 127) * 100  # VAB `[0, 127]` -> SFZ cents.
-                sfz_portamento_time = (tone.portamento_time / 127) * 0.5 # Max 0.5 seconds glide.
-                # @todo portamento width?
-                sfz_bend_up         = tone.pitch_bend_max * 100
-                sfz_bend_down       = tone.pitch_bend_min * 100
-                sfz_pan             = round((tone.panning - 64) / 64 * 100) # VAB (0..64..127) -> SFZ (-100..0..100)
-                sfz_fine_tune       = round((tone.shift / 128) * 100)       # VAB `[0, 127]` -> SFZ cents.
-                sfz_loop            = "loop_mode=loop_continuous " if tone.mode == ToneMode.REVERB else ""
-                sfz_attack          = convert_psx_rate_to_sec((tone.adsr1 >> 10) & 0x1F, 0x1F)
-                sfz_decay           = convert_psx_rate_to_sec(dr, 0x0F)
-                sfz_sustain         = convert_psx_sl_to_percent(sl)
-                sfz_decay2          = f"ampeg_decay2={convert_psx_rate_to_sec(sr, 0x7F)} ampeg_sustain2=0 " if sr > 0 else ""
-                sfz_release         = convert_psx_rate_to_sec(rr, 0x1F)
-
-                # @todo
-                # Handle the 'Sustain Rate' (SR)
-                # If SR is 0, the note holds forever until release. 
-                # If SR > 0, it slowly fades out during the hold.
-                #sfz_hold_decay = _psx_rate_to_sec(sr, 0x7F) if sr > 0 else 100.0
-                # If SR > 0, the sound fades toward silence (0%) during the sustain phase.
-
-                # Write SFZ data.
-                wav_name = f"{SAMPLES_FOLDER}/{VAG_PREFIX}{tone.vag_id:03}.WAV"
-                output.write(
-                    f"<region> "
-                    f"sample={wav_name} "
-                    f"bank=0 "
-                    f"program={tone.program_idx} "
-                    f"lokey={tone.note_min} "
-                    f"hikey={tone.note_max} "
-                    #f"pitchlfo_freq={sfz_vibrato_freq} "
-                    #f"pitchlfo_depth={sfz_vibrato_depth} "
-                    #f"pitchlfo_delay=0.1 " # Adding a tiny delay makes it sound more natural
-                    #f"oppua_portamento={sfz_portamento_time} "
-                    f"bend_up={sfz_bend_up} "
-                    f"bend_down={sfz_bend_down} "
-                    f"pitch_keycenter={tone.center} "
-                    f"volume={tone.volume} "
-                    f"pan={sfz_pan} "
-                    f"tune={sfz_fine_tune} "
-                    #f"ampeg_attack={sfz_attack} "
-                    #f"ampeg_decay={sfz_decay} "
-                    #f"ampeg_sustain={sfz_sustain} "
-                    #f"{sfz_decay2}"
-                    #f"ampeg_release={sfz_release} "
-                    f"{sfz_loop}\n"
-                )
 
 def _convert_sfz_to_sf2(convert_sound_bank_py: Path, output_folder: Path, sfz_file: Path):
     """
