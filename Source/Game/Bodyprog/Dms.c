0521d9cf8f5efc1f2effe9a64b977b4dd7f1b24f
Sezz <sezzary@outlook.com>
2026-08-07 20:14:25 +1000

    Rough port of DMS code

---
+++ Source/Game/Bodyprog/Dms.c
@@ -0,0 +1,248 @@
+#include "Framework.h"
+#include "Psx.h"
+#include "Game/Bodyprog/Bodyprog.h"
+#include "Game/Bodyprog/Dms.h"
+
+//#include "Bodyprog/ItemScreens.h"
+#include "Bodyprog/Screen/ScreenDraw.h"
+#include "Bodyprog/Sound/SoundSystem.h"
+#include "Main/FsQueue.h"
+
+#include "Application.h"
+#include "Assets/AssetStreamer.h"
+
+using namespace Silent::Assets;
+
+namespace Silent::Game
+{
+    void Dms_CharacterTransformGet(VECTOR3* pos, SVECTOR3* rot, const std::string& charaName, q19_12 time, const Asset& dmsAsset)
+    {
+        int charaEntryIdx = Dms_CharacterGetIdxByName(charaName, dmsAsset);
+        if (charaEntryIdx == NO_VALUE)
+        {
+            // Character not found in DMS.
+            Math_Vector3Zero(pos);
+            Math_SVectorZero(rot);
+
+            Text_Debug_PositionSet(SCREEN_POSITION_X(15.75f), SCREEN_POSITION_Y(37.5f));
+
+    #if VERSION_EQUAL_OR_OLDER(PROTO_981216)
+            // Code seen in 98-12-16 build.
+            Text_Debug_Draw(charaName);
+            Text_Debug_Draw(" doesn't exist in dms.");
+    #endif
+        }
+        else
+        {
+            Dms_CharacterTransformGetByIdx(pos, rot, charaEntryIdx, time, dmsAsset);
+        }
+    }
+
+    int Dms_CharacterGetIdxByName(std::string& charaName, const Asset& dmsAsset)
+    {
+        const auto& dmsData = dmsAsset.GetData<DmsAsset>();
+
+        // Find matching name.
+        for (int i = 0; i < dmsData.CharacterEntryCount; i++)
+        {
+            if (!strncmp(charaName.c_str(), dmsData.CharacterEntries[i].Name.c_str(), 4))
+            {
+                return i;
+            }
+        }
+
+        return NO_VALUE;
+    }
+
+    void Dms_CharacterTransformGetByIdx(VECTOR3* pos, SVECTOR3* rot, int charaIdx, q19_12 time, const Asset& dmsAsset)
+    {
+        const auto& dmsData = dmsAsset.GetData<DmsAsset>();
+
+        const auto& charaEntry = dmsData.CharacterEntries[charaIdx];
+
+        // Get keyframe interpolation data.
+        int    prevKeyframeIdx = 0;
+        int    nextKeyframeIdx = 0;
+        q19_12 alpha           = Q12(0.0f);
+        Dms_KeyframeInterpGet(prevKeyframeIdx, nextKeyframeIdx, alpha, time, charaEntry, dmsData);
+
+        // Interpolate frame.
+        auto        activeCharaKeyframe = DmsKeyframeCharacter{};
+        const auto& charaKeyframes      = charaEntry.Keyframes.Character;
+        Dms_CharacterKeyframeInterpolate(activeCharaKeyframe,
+                                         charaKeyframes[prevKeyframeIdx], charaKeyframes[nextKeyframeIdx],
+                                         alpha);
+
+        // Set position.
+        pos->vx = Q8_TO_Q12(activeCharaKeyframe.Position.x + dmsData.Origin.x);
+        pos->vy = Q8_TO_Q12(activeCharaKeyframe.Position.y + dmsData.Origin.y);
+        pos->vz = Q8_TO_Q12(activeCharaKeyframe.Position.z + dmsData.Origin.z);
+
+        // Set rotation.
+        rot->vx = activeCharaKeyframe.Rotation.x;
+        rot->vy = activeCharaKeyframe.Rotation.y;
+        rot->vz = activeCharaKeyframe.Rotation.z;
+    }
+
+    void Dms_CharacterKeyframeInterpolate(DmsKeyframeCharacter& result,
+                                          const DmsKeyframeCharacter& prevKeyframe, const DmsKeyframeCharacter& nextKeyframe,
+                                          q19_12 alpha)
+    {
+        // Low-precision lerp between positions.
+        result.Position.x = prevKeyframe.Position.x + Q12_MULT_PRECISE(nextKeyframe.Position.x - prevKeyframe.Position.x, alpha);
+        result.Position.y = prevKeyframe.Position.y + Q12_MULT_PRECISE(nextKeyframe.Position.y - prevKeyframe.Position.y, alpha);
+        result.Position.z = prevKeyframe.Position.z + Q12_MULT_PRECISE(nextKeyframe.Position.z - prevKeyframe.Position.z, alpha);
+
+        // High-precision lerp between rotations.
+        result.Rotation.x = Dms_AngleLerp(prevKeyframe.Rotation.x, nextKeyframe.Rotation.x, alpha);
+        result.Rotation.y = Dms_AngleLerp(prevKeyframe.Rotation.y, nextKeyframe.Rotation.y, alpha);
+        result.Rotation.z = Dms_AngleLerp(prevKeyframe.Rotation.z, nextKeyframe.Rotation.z, alpha);
+    }
+
+    s32 Dms_CameraTargetsGet(VECTOR3* posTarget, VECTOR3* lookAtTarget, q3_12* unusedAngle, q19_12 time,
+                            const Asset* dmsAsset)
+    {
+        s32                 prevKeyframeIdx;
+        s32                 nextKeyframeIdx;
+        s32                 alpha;
+        s32                 camProjVal;
+
+        const auto& dmsData = asset.GetData<DmsAsset>();
+
+        const s_DmsEntry& camEntry = dmsHdr.CameraEntry;
+
+        // Interpolate current keyframe.
+        auto activeCamKeyframe = s_DmsKeyframeCamera{};
+        Dms_KeyframeInterpGet(&prevKeyframeIdx, &nextKeyframeIdx, &alpha, time, camEntry, dmsHdr);
+        camProjVal = Dms_CameraKeyframeLerp(&activeCamKeyframe, camEntry.Keyframes.Camera[prevKeyframeIdx],
+                                            camEntry.Keyframes.Camera[nextKeyframeIdx],
+                                            alpha);
+
+        // Set position target.
+        posTarget->vx = Q8_TO_Q12(activeCamKeyframe.PositionTarget.x + dmsData.Origin.x);
+        posTarget->vy = Q8_TO_Q12(activeCamKeyframe.PositionTarget.y + dmsData.Origin.y);
+        posTarget->vz = Q8_TO_Q12(activeCamKeyframe.PositionTarget.z + dmsData.Origin.z);
+
+        // Set look-at target.
+        lookAtTarget->vx = Q8_TO_Q12(activeCamKeyframe.LookAtTarget.x + dmsData.Origin.x);
+        lookAtTarget->vy = Q8_TO_Q12(activeCamKeyframe.LookAtTarget.y + dmsData.Origin.y);
+        lookAtTarget->vz = Q8_TO_Q12(activeCamKeyframe.LookAtTarget.z + dmsData.Origin.z);
+
+        // `camProjVal` comes from `curFrame.projectionDistance`, return value is passed to `vcChangeProjectionValue`.
+        // Might be related to FOV?
+        return camProjVal;
+    }
+
+    s32 Dms_CameraKeyframeLerp(DmsKeyframeCamera& result,
+                               const DmsKeyframeCamera& prevKeyframe, const DmsKeyframeCamera& nextKeyframe,
+                               q19_12 alpha)
+    {
+        // Set position target.
+        result.PositionTarget.vx = prevKeyframe.PositionTarget.vx + Q12_MULT_PRECISE(nextKeyframe.PositionTarget.vx - prevKeyframe.PositionTarget.vx, alpha);
+        result.PositionTarget.vy = prevKeyframe.PositionTarget.vy + Q12_MULT_PRECISE(nextKeyframe.PositionTarget.vy - prevKeyframe.PositionTarget.vy, alpha);
+        result.PositionTarget.vz = prevKeyframe.PositionTarget.vz + Q12_MULT_PRECISE(nextKeyframe.PositionTarget.vz - prevKeyframe.PositionTarget.vz, alpha);
+
+        // Set look-at target.
+        result.LookAtTarget.vx = prevKeyframe.LookAtTarget.vx + Q12_MULT_PRECISE(nextKeyframe.LookAtTarget.vx - prevKeyframe.LookAtTarget.vx, alpha);
+        result.LookAtTarget.vy = prevKeyframe.LookAtTarget.vy + Q12_MULT_PRECISE(nextKeyframe.LookAtTarget.vy - prevKeyframe.LookAtTarget.vy, alpha);
+        result.LookAtTarget.vz = prevKeyframe.LookAtTarget.vz + Q12_MULT_PRECISE(nextKeyframe.LookAtTarget.vz - prevKeyframe.LookAtTarget.vz, alpha);
+
+        // Set projection distance.
+        result.ProjectionDistance = prevKeyframe.ProjectionDistance + Q12_MULT_PRECISE(nextKeyframe.ProjectionDistance - prevKeyframe.ProjectionDistance, alpha);
+
+        return result.ProjectionDistance;
+    }
+
+    void Dms_KeyframeInterpGet(int& prevKeyframeIdx, int& nextKeyframeIdx, q19_12& alpha, q19_12 time,
+                               const DmsEntry camEntry, const Asset& asset)
+    {
+        int prevFrameIdx = 0;
+        int nextFrameIdx = 0;
+
+        // Define playback frames and set interpolation alpha.
+        switch (Dms_SegmentStateGet(time, asset))
+        {
+            case DmsSegmentState_Interpolating:
+                prevFrameIdx = FP_FROM(time, Q12_SHIFT);
+                nextFrameIdx = prevFrameIdx + 1;
+                alpha        = Q12_FRACT(time);
+                break;
+
+            case DmsSegmentState_SingleFrame:
+                prevFrameIdx = FP_FROM(time, Q12_SHIFT);
+                nextFrameIdx = prevFrameIdx;
+                alpha        = Q12(0.0f);
+                break;
+
+            case DmsSegmentState_Ending:
+                prevFrameIdx = FP_FROM(time, Q12_SHIFT) - 1;
+                nextFrameIdx = prevFrameIdx + 1;
+                alpha        = Q12_FRACT(time) + Q12(1.0f);
+                break;
+        }
+
+        // Set previous and next keyframe indices.
+        prevKeyframeIdx = Dms_KeyframeIdxGet(prevFrameIdx, camEntry);
+        nextKeyframeIdx = Dms_KeyframeIdxGet(nextFrameIdx, camEntry);
+    }
+
+    e_DmsSegmentState Dms_SegmentStateGet(q19_12 time, const Asset& dmsAsset)
+    {
+        const auto& dmsData = dmsAsset.GetData<DmsAsset>();
+
+        int frameIdx = FP_FROM(time, Q12_SHIFT);
+
+        // Run through segments.
+        for (const auto& curSegment: dmsData.Segments)
+        {
+            // Check if at end playback frame.
+            if (frameIdx != ((curSegment.StartFrameIdx + curSegment.FrameCount) - 1))
+            {
+                continue;
+            }
+
+            if (curSegment.FrameCount > 1)
+            {
+                return DmsSegmentState_Ending;
+            }
+
+            return DmsSegmentState_SingleFrame;
+        }
+
+        return DmsSegmentState_Interpolating;
+    }
+
+    int Dms_KeyframeIdxGet(int frameIdx, const DmsEntry& entry)
+    {
+        int keyframeIdx = frameIdx;
+
+        // Run through ranges to freeze or offset keyframe.
+        for (const auto& curHoldRange : entry-.HoldRanges)
+        {
+            // Check if target playback frame falls before hold range.
+            if (frameIdx < curHoldRange.StartFrameIdx)
+            {
+                break;
+            }
+
+            // If target playback frame is within hold range, freeze at mapped keyframe.
+            if (frameIdx <= curHoldRange.EndFrameIdx)
+            {
+                keyframeIdx = curHoldRange.KeyframeIdx;
+                break;
+            }
+
+            // Align keyframe index.
+            keyframeIdx -= curHoldRange.EndFrameIdx - curHoldRange.StartFrameIdx;
+        }
+
+        // Clamp keyframe index.
+        int clampedKeyframeIdx = CLAMP(keyframeIdx, 0, entry.KeyframeCount - 1);
+        return clampedKeyframeIdx;
+    }
+
+    q19_12 Dms_AngleLerp(q3_12 angleFrom, q3_12 angleTo, q19_12 alpha)
+    {
+        return Q12_ANGLE_NORM_S((q19_12)(Q12_MULT_PRECISE(Q12_ANGLE_NORM_S(angleTo - angleFrom), alpha)) + angleFrom);
+    }
+}
--- Source/Game/Bodyprog/Dms.h
+++ Source/Game/Bodyprog/Dms.h
@@ -1,90 +1,112 @@
 #pragma once
 
-namespace Silent::Gam
+// @todo temp
+#include "Assets/AssetStreamer.h"
+using namespace Silent::Assets;
+
+namespace Silent::Game
 {
-    /** @brief DMS cutscene camera keyframe. */
-    struct s_DmsKeyframeCamera
+    /** @brief DMS cutscene segment states. */
+    enum e_DmsSegmentState
     {
-        SVECTOR3 positionTarget; /** Q7.8 */
-        SVECTOR3 lookAtTarget;   /** Q7.8 */
-        s16      field_C[2];     // `field_C[1]` gets passed to `vcChangeProjectionValue`.
+        DmsSegmentState_Interpolating = 0,
+        DmsSegmentState_SingleFrame   = 1,
+        DmsSegmentState_Ending        = 2
     };
 
-    /** @brief DMS cutscene character transform keyframe. */
-    struct s_DmsKeyframeCharacter
-    {
-        SVECTOR3 position; /** Q7.8 */
-        SVECTOR3 rotation; /** Q7.8 */
-    };
+    /** @brief Gets a character transformation at a given playback time from a DMS header by entry name.
+     *
+     * @param pos Output character position.
+     * @param rot Output character rotation.
+     * @param charaName DMS character entry name.
+     * @param time Playback time.
+     * @param dmsAsset DMS aset.
+     */
+    void Dms_CharacterTransformGet(VECTOR3* pos, SVECTOR3* rot, const std::string& charaName, q19_12 time, const Asset& dmsAsset);
 
-    /** @brief DMS cutscene entry. */
-    struct s_DmsEntry
-    {
-        s16       keyframeCount_0;
-        u8        svectorCount_2; /** `svectors_8` array size. */
-        u8        field_3;        // Usually 0, but sometimes filled in, possibly junk data left in padding byte.
-        char      name_4[4];      // First 4 chars of name. E.g. Code checks for "DAHLIA", file is "DAHL".
-        SVECTOR3* svectors_8;     // Pointer to `SVECTOR3`s. Unknown purpose.
-        union
-        {
-            s_DmsKeyframeCharacter* character;
-            s_DmsKeyframeCamera*    camera;
-        } keyframes_C;
-    };
+    /** @brief Gets a DMS character entry index by name.
+     *
+     * @param charaName Name of the DMS character entry to find.
+     * @param dmsAsset DMS header.
+     * @return DMS character entry index.
+     */
+    int Dms_CharacterGetIdxByName(std::string& charaName, const Asset& dmsAsset);
 
-    /** @brief DMS cutscene interval */
-    struct s_DmsInterval
-    {
-        s16 startKeyframeIdx;
-        s16 frameCount; /** Frame duration at 30 FPS. */
-    };
+    /** @brief Gets a character transformation at a given playback time from a DMS header by entry index.
+     *
+     * @param pos Output character position.
+     * @param rot Output character rotation.
+     * @param charaIdx DMS character entry index.
+     * @param time Playback time.
+     * @param dmsAsset DMS header.
+     */
+    void Dms_CharacterTransformGetByIdx(VECTOR3* pos, SVECTOR3* rot, int charaIdx, q19_12 time, const Asset& dmsAsset);
 
-    /** @brief DMS cutscene file header. */
-    struct s_DmsHeader
-    {
-        u8             isLoaded; /** `bool` */
-        u8             characterCount;
-        u8             intervalCount;
-        u8             field_3; // Usually 0, but sometimes filled in.
-        u32            field_4; // Unknown, correlates with DMS file size.
-        s_DmsInterval* intervals;
-        VECTOR3        origin; /** Q23.8 | Origin position. Gets added to character positions. */
-        s_DmsEntry*    characters;
-        s_DmsEntry     camera;
-    };
+    /** @brief Linearly interpolates between two DMS character keyframes.
+     *
+     * @param result Interpolation result.
+     * @param prevKeyframe Previous character keyframe.
+     * @param nextKeyframe Next character keyframe.
+     * @param alpha Interpolation alpha.
+     */
+    void Dms_CharacterKeyframeInterpolate(DmsKeyframeCharacter& result,
+                                          const DmsKeyframeCharacter& prevKeyframe, const DmsKeyframeCharacter& nextKeyframe,
+                                          q19_12 alpha);
 
-    void DmsHeader_FixOffsets(s_DmsHeader* dmsHdr);
+    /** @brief Gets the camera position and look-at targets from a DMS header.
+     *
+     * @param posTarget Output camera position target.
+     * @param lookAtTarget Output camera look-at target.
+     * @param unusedAngle @unused
+     * @param time Playback time.
+     * @param dmsAsset DMS header.
+     */
+    s32 Dms_CameraTargetsGet(VECTOR3* posTarget, VECTOR3* lookAtTarget, q3_12* unusedAngle, q19_12 time,
+                            const DmsAsset* dmsAsset);
 
-    void DmsEntry_FixOffsets(s_DmsEntry* entry, s_DmsHeader* dmsHdr);
+    /** @brief Linearly interpolates between two DMS camera keyframes.
+     *
+     * @param result Interpolation result.
+     * @param prevKeyframe Previous camera keyframe.
+     * @param nextKeyframe Next camera keyframe.
+     * @param alpha Interpolation alpha.
+     */
+    s32 Dms_CameraKeyframeLerp(DmsKeyframeCamera& result,
+                               const DmsKeyframeCamera& prevKeyframe, const DmsKeyframeCamera& nextKeyframe,
+                               q19_12 alpha);
 
-    /** @unused? `volatile` needed for match. */
-    s_DmsInterval* func_8008CA60(volatile s32 unused, s32 idx, s_DmsHeader* dmsHdr);
+    /** @brief Gets keyframe interpolation data at a given playback time from a given DMS camera entry.
+     *
+     * @param prevKeyframeIdx Output previous keyframe index.
+     * @param nextKeyframeIdx Output next keyframe index.
+     * @param alpha Output interpolation alpha.
+     * @param time Playback time.
+     * @param camEntry DMS camera entry.
+     * @param dmsAsset DMS header.
+     */
+    void Dms_KeyframeInterpGet(int& prevKeyframeIdx, int& nextKeyframeIdx, q19_12& alpha, q19_12 time,
+                               const DmsEntry& camEntry, const Asset& dmsAsset);
 
-    void Dms_CharacterGetPosRot(VECTOR3* pos, SVECTOR3* rot, const char* charaName, q19_12 time, s_DmsHeader* dmsHdr);
+    /** @brief Gets the state of a segment at a given playback time from a DMS header.
+     *
+     * @param time Playback time.
+     * @param dmsAsset DMS header.
+     * @return DMS segment state.
+     */
+    e_DmsSegmentState Dms_SegmentStateGet(q19_12 time, const Asset& dmsAsset);
 
-    void Dms_CharacterGetStartPosRot(VECTOR3* pos, SVECTOR3* rot, const char* charaName, s32 time, s_DmsHeader* dmsHdr);
+    /** @brief Gets a keyframe index at a given playback frame from a DMS entry.
+     *
+     * @param frameIdx Playback frame index.
+     * @param entry DMS entry.
+     */
+    int Dms_KeyframeIdxGet(int frameIdx, const DmsEntry& entry);
 
-    s32 Dms_CharacterFindIdxByName(char* name, s_DmsHeader* dmsHdr);
-
-    void Dms_CharacterGetPosRotByIdx(VECTOR3* pos, SVECTOR3* rot, s32 charaIdx, q19_12 time, s_DmsHeader* dmsHdr);
-
-    void Dms_CharacterKeyframeInterpolate(s_DmsKeyframeCharacter* result, s_DmsKeyframeCharacter* frame0, s_DmsKeyframeCharacter* frame1, s32 alpha);
-
-    /** @unused? Returns `96 * cotangent(angle / 2)`. Possibly camera/FOV related. */
-    q3_12 func_8008CDBC(q3_12 angle);
-
-    s32 Dms_CameraGetTargetPos(VECTOR3* posTarget, VECTOR3* lookAtTarget, u16* arg2, q19_12 time, s_DmsHeader* dmsHdr);
-
-    /** @unused Returns whether any axis differs by more than 22.5 degrees (1/16 of full rotation). */
-    bool func_8008CF54(SVECTOR3* rot0, SVECTOR3* rot1);
-
-    s32 Dms_CameraKeyframeInterpolate(s_DmsKeyframeCamera* result, const s_DmsKeyframeCamera* frame0, const s_DmsKeyframeCamera* frame1, s32 alpha);
-
-    void func_8008D1D0(s32* prevKeyframe, s32* nextKeyframe, q19_12* alpha, q19_12 time, s_DmsEntry* camEntry, s_DmsHeader* dmsHdr);
-
-    u32 Dms_IntervalStateGet(q19_12 time, s_DmsHeader* dmsHdr);
-
-    s32 func_8008D330(s32 arg0, s_DmsEntry* camEntry);
-
-    s32 Math_LerpFixed12(s16 from, s16 to, q19_12 alpha);
+    /** @brief Linearly interpolates between two angles.
+     *
+     * @param angleFrom Angle from.
+     * @param angleTo Angle to.
+     * @param alpha Interpolation alpha.
+     */
+    q19_12 Dms_AngleLerp(q3_12 angleFrom, q3_12 angleTo, q19_12 alpha);
 }
